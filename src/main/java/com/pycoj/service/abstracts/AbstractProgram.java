9d864e119de9077f6116c08abe6ac1d04aaf83a7
pycgaoshou <losangels03@163.com>
2017-06-16 09:18:54 +0800

    单个输入输出用例变更为多个输入输出用例，修改为循环获取，返回输入流数组

--- src/main/java/com/pycoj/service/abstracts/AbstractProgram.java
+++ src/main/java/com/pycoj/service/abstracts/AbstractProgram.java
@@ -1,21 +1,42 @@
 package com.pycoj.service.abstracts;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.InputStream;
+import java.io.*;
 
 /**
  * Created by Heyman on 2017/6/12.
  */
 public abstract class AbstractProgram implements Program{
+    /**
+     * 获取输入用例的输入流
+     * @param inputFileDir 输入用例所在路径
+     * @return
+     * @throws FileNotFoundException
+     */
     @Override
-    public InputStream getInput(String inputFileDir) throws FileNotFoundException {
-        return new FileInputStream(inputFileDir+"/input.txt");
+    public InputStream[] getInput(String inputFileDir) throws FileNotFoundException {
+        File InputFileDir=new File(inputFileDir);
+        int size=InputFileDir.list().length;
+        InputStream[] streams=new InputStream[size/2];
+        for (int i=1;i<=size/2;i++){
+            streams[i-1]=new FileInputStream(inputFileDir+"/input"+i+".txt");
+        }
+        return streams;
     }
 
+    /**
+     * 获取输出用例的输入流
+     * @param outputFileDir 输出用例所在路径
+     * @return
+     * @throws FileNotFoundException
+     */
     @Override
-    public InputStream getOutput(String outputFileDir) throws FileNotFoundException {
-        return new FileInputStream(outputFileDir+"/output.txt");
+    public InputStream[] getOutput(String outputFileDir) throws FileNotFoundException {
+        File InputFileDir=new File(outputFileDir);
+        int size=InputFileDir.list().length;
+        InputStream[] streams=new InputStream[size/2];
+        for (int i=1;i<=size/2;i++){
+            streams[i-1]=new FileInputStream(outputFileDir+"/output"+i+".txt");
+        }
+        return streams;
     }
 }
